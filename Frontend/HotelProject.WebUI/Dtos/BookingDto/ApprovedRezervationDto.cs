03edc6947435d58f6a08ec7ab5a26203d148dcdd
Metin Yeni <metinyeni33@gmail.com>
2023-08-29 00:14:20 +0300

    Add project files.

---
+++ Frontend/HotelProject.WebUI/Dtos/BookingDto/ApprovedRezervationDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HotelProject.WebUI.Dtos.BookingDto
+{
+    public class ApprovedRezervationDto
+    {
+        public int BookingID { get; set; }
+        public string Status { get; set; }
+    }
+}
